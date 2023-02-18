dee1aca543dc3b39c9b43f69d6654c793dfc7531
jdittert <jondittert@gmail.com>
2023-02-18 11:33:59 -0500

    Code operate function

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+// Operate function
+function operate (a, opt, b) {
+    if (opt === "+") return add (a, b);
+    if (opt === "-") return subtract (a, b);
+    if (opt === "*") return multiply (a, b);
+    if (opt === "/") return divide (a, b);
+}
